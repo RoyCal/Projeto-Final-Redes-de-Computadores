d00bdd5b864c2768692eae7dc47732d624d78ac7
Vito Elias <vito.elias9@gmail.com>
2024-10-11 14:40:22 -0300

    comments

--- clienteUDP.py
+++ clienteUDP.py
@@ -69,6 +69,6 @@ byte_3 = data[3] # o quarto byte não precisa ser convertido pois corresponde ao
 print("Tipo da mensagem:", byte_0[:4], "| Tipo da requisição:", byte_0[4:]) # a primeira metade do primeiro byte é o tipo da mensagem e a segunda metade é o tipo da requisição
 print("Indentificador:", int(byte_1 + byte_2, 2)) # os dois bytes seguintes, isto é, o segundo e o terceiro, correspondem ao identificador
 print("Tamanho da mensagem:", byte_3) # o quarto byte é o tamanho da mensagem
-print("Mensagem:", data[4:4 + byte_3].decode()) # do quinto byte em diante temos a mensagem, mas só pegamos o tamanho que o servidor forneceu...
+print("Mensagem:", data[4:4 + byte_3].decode()) # do quinto byte em diante temos a mensagem, mas só pegamos o tamanho que o servidor forneceu no tamanho da mensagem...
 
 clientSocket.close() # fecha a conexão
