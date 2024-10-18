722ec5db650f5837787daa4ffcd995fedf6c5809
Vito Elias <vito.elias9@gmail.com>
2024-10-18 12:58:10 -0300

    fix checksum

--- clienteScapy.py
+++ clienteScapy.py
@@ -1,14 +1,18 @@
 from scapy.layers.inet import IP, UDP
-from scapy.layers.l2 import Ether
 from scapy.sendrecv import srp1
 
-pacote = IP(dst="15.228.191.109", len=0x000b)
-pacote /= UDP(dport=50000, chksum=0x0000, len=0x000b)
+pacote = IP(dst="15.228.191.109")
+pacote /= UDP(dport=50000, chksum=0x0000)
 pacote /= "\x00\x57\x22"
 
 hex_string = ''.join(f'\\x{byte:02x}' for byte in bytes(pacote))
 
-byte_list = [int(hex_string[i:i+2], 16) for i in range(2, len(hex_string), 4)]
+if((len(hex_string) / 4) % 2 != 0):
+    hex_string += "\\x00"
+
+print(hex_string)
+
+byte_list = [int(hex_string[i:i+2] + hex_string[i+4:i+6], 16) for i in range(2, len(hex_string), 8)]
 
 soma = hex(sum(byte_list))
 
@@ -25,8 +29,8 @@ print(soma)
 
 print(checksum)
 
-pacote = IP(dst="15.228.191.109", len=0x000b)
-pacote /= UDP(dport=50000, chksum=int(checksum, 16), len=0x000b)
+pacote = IP(dst="15.228.191.109")
+pacote /= UDP(dport=50000, chksum=int(checksum, 16))
 pacote /= "\x00\x57\x22"
 
 resultado = srp1(pacote)
