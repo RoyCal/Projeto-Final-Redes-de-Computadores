4b9c2b10e6db26c4f9032f417abc2aaf71002244
Vito Elias <vito.elias9@gmail.com>
2024-10-18 13:42:07 -0300

    test

--- clienteScapy.py
+++ clienteScapy.py
@@ -1,9 +1,11 @@
 from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether
 from scapy.sendrecv import srp1
 
-pacote = IP(dst="15.228.191.109")
-pacote /= UDP(dport=50000, chksum=0x0000)
-pacote /= "\x00\x57\x22"
+pacote = Ether()
+pacote /= IP(dst="15.228.191.109", len=0x001f, chksum=0x0000, ttl=128)
+pacote /= UDP(sport=62719, dport=50000, chksum=0x0000, len=0x000b)
+pacote /= "\x00\xf0\x59"
 
 hex_string = ''.join(f'\\x{byte:02x}' for byte in bytes(pacote))
 
@@ -29,9 +31,12 @@ print(soma)
 
 print(checksum)
 
-pacote = IP(dst="15.228.191.109")
-pacote /= UDP(dport=50000, chksum=int(checksum, 16))
-pacote /= "\x00\x57\x22"
+pacote = Ether()
+pacote /= IP(dst="15.228.191.109", len=0x001f, chksum=0x0000, ttl=128, id=48085)
+pacote /= UDP(sport=62719, dport=50000, chksum=int("9f1d", 16), len=0x000b)
+pacote /= "\x00\xf0\x59"
+
+pacote.show()
 
 resultado = srp1(pacote)
 
